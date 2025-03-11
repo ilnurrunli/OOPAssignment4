6f1e89d70095f4c7410fba0dd205b9389818e690
crazypon <i.yamaletdino@newuu.uz>
2025-03-11 17:41:01 +0500

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include "MyStack1.h"
 #include "MyStack2.h"
+using namespace std;
 
 int main() {
     MyStack1 s1;
     MyStack2 s2;
     int num;
 
-    std::cout << "Enter values (use -1 for backspace, -99 to stop): ";
+    cout << "Enter values (use -1 for backspace, -99 to stop): ";
     while (true) {
-        std::cin >> num;
-        if (num == -99) break; // Stop input
-
+        cin >> num;
+        if (num == -99) break; // Stop input when -99 is entered
         if (num == -1) { // Handle backspace
             if (!s1.isEmpty()) s1.pop();
             if (!s2.isEmpty()) s2.pop();
@@ -20,11 +20,10 @@ int main() {
             s2.push(num);
         }
     }
-
     // Output results from both stacks
-    std::cout << "MyStack1: ";
+    cout << "MyStack1: ";
     s1.printStack();
-    std::cout << "MyStack2: ";
+        cout << "MyStack2: ";
     s2.printStack();
 
     return 0;
